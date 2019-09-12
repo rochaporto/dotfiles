a97fc6be587175eec28348324c1e6da2ffae2cf8
Ricardo Rocha <rocha.porto@gmail.com>
2019-09-12 18:43:01 +0200

    Update i3 metar command

--- .i3/layout-work.sh
+++ .i3/layout-work.sh
@@ -9,9 +9,9 @@ google-chrome --new-window mmm.cern.ch/owa its.cern.ch/jira/browse/OS &
 
 i3-msg 'workspace 2; append_layout ~/.i3/workspace-2.json'
 
-sleep 10
+sleep 3
 
-terminator -r metar -x bash -c 'while true; do setterm -cursor off; clear; echo -e \"\e[33m$(curl -s https://tgftp.nws.noaa.gov/data/observations/metar/stations/LSGG.TXT)\"; sleep 600; done'
 terminator -r clock -x tty-clock -c -C 3 -r &
+terminator -r metar -x 'while true; do setterm -cursor off; clear; echo -e "\e[33m$(curl -s https://tgftp.nws.noaa.gov/data/observations/metar/stations/LSGG.TXT)"; sleep 300; done'
 terminator -r mutt -x mutt &
 google-chrome --new-window mattermost.web.cern.ch &
