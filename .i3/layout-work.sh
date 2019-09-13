85c8025233c86f0c8c5d7ae7a92ffc183464d84d
Ricardo Rocha <rocha.porto@gmail.com>
2019-09-13 09:53:51 +0200

    Fix i3 layout work startup

--- .i3/layout-work.sh
+++ .i3/layout-work.sh
@@ -7,9 +7,9 @@ terminator -r t2 &
 terminator -r t3 &
 google-chrome --new-window mmm.cern.ch/owa its.cern.ch/jira/browse/OS &
 
-i3-msg 'workspace 2; append_layout ~/.i3/workspace-2.json'
+sleep 2
 
-sleep 3
+i3-msg 'workspace 2; append_layout ~/.i3/workspace-2.json'
 
 terminator -r clock -x tty-clock -c -C 3 -r &
 terminator -r metar -x 'while true; do setterm -cursor off; clear; echo -e "\e[33m$(curl -s https://tgftp.nws.noaa.gov/data/observations/metar/stations/LSGG.TXT)"; echo -e "\n\e[94m$(curl -s https://tgftp.nws.noaa.gov/data/forecasts/taf/stations/LSGG.TXT)"; sleep 600; done'
